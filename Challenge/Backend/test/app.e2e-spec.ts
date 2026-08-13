5b201b03a242c7e512cedd3582dce08fafa6d9b9
colinrdickinson-code <colin.r.dickinson@gmail.com>
2026-08-13 10:20:18 -0400

    Fix spelling errors

--- Challenge/Backend/test/app.e2e-spec.ts
+++ Challenge/Backend/test/app.e2e-spec.ts
@@ -42,7 +42,7 @@ describe('Backend API (e2e)', () => {
     .expect(401);
   });
 
-  it('/albums (GET) should return the autheticated user\'s albums', async () => {
+  it('/albums (GET) should return the authenticated user\'s albums', async () => {
     const loginRespone = await request(app.getHttpServer())
     .post('/auth/login')
     .send({
